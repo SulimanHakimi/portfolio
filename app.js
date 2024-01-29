3910cfe392b7788b12168aa5abc7fd9a81607a03
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-01-29 06:15:25 -0800

    fix

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
-import projects from "./data.js";
+let projectCard = [[
+  {
+    id: "2",
+    projectName: "HomeTown",
+    imageLink: "./img/homwtown.jpeg",
+    githubLink: "https://github.com/SulimanHakimi/scrimba_homeTown.git",
+    liveDemoLink: "https://sulimanhakimi.github.io/scrimba_homeTown/",
+  },
+  {
+    id: "3",
+    projectName: "Calculator",
+    imageLink: "./img/calculator.jpeg",
+    githubLink: "https://github.com/SulimanHakimi/calculator_1.git",
+    liveDemoLink: "https://sulimanhakimi.github.io/calculator_1/",
+  },
+  {
+    id: "4",
+    projectName: "Random Quote Machine",
+    imageLink: "./img/random.jpeg",
+    githubLink:
+      "https://github.com/SulimanHakimi/Build-Random-Quote-Machine-FreeCodeCamp.git",
+    liveDemoLink:
+      "https://sulimanhakimi.github.io/Build-Random-Quote-Machine-FreeCodeCamp/",
+  },
+  {
+    id: "1",
+    projectName: "Landing Page",
+    imageLink: "./img/landing.jpeg",
+    githubLink: "https://github.com/SulimanHakimi/assignment_1.git",
+    liveDemoLink: "https://sulimanhakimi.github.io/landing-page/",
+  },]
+];
 let cardsList = document.querySelector(".cards-list");
-
-fetch("https://api.example.com/data")
-  .then((response) => response.json())
-  .then((projects) => console.log(projects))
-  .catch((error) => console.error("Error:", error));
-projects.forEach((projects) => {
-  //    cardsList.innerHTML =
-  console.log(projects);
+projectCard.forEach((project) => {
+    console.log(project)
+  cardsList.innerHTML = `
+    <div class="card">
+    <img src="${project.imageLink}" alt="" />
+    <span>Landing Page</span>
+    <div class="buttons">
+      <a target="_blank" href="${project.imageLink}">GitHub</a>
+      <a target="_blank" href="${project.data}">Live Demo</a>
+    </div>
+  </div>
+    `;
 });
-// `
-//    <div class="card">
-//    <img src="${project.imageLink}" alt="" />
-//    <span>Landing Page</span>
-//    <div class="buttons">
-//      <a target="_blank" href="${project.githubLink}">GitHub</a>
-//      <a target="_blank" href="${project.liveDemoLink}">Live Demo</a>
-//    </div>
-//  </div>
-//    `
