2d21e3e43059d402d845ba46147398eba481c306
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-01-29 01:48:18 -0800

    clean code and add json file

---
+++ app.js
@@ -0,0 +1,21 @@
+import projects from "./data.js";
+let cardsList = document.querySelector(".cards-list");
+
+fetch("https://api.example.com/data")
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error("Error:", error));
+projects.forEach((projects) => {
+  //    cardsList.innerHTML =
+  console.log(projects);
+});
+// `
+//    <div class="card">
+//    <img src="${project.imageLink}" alt="" />
+//    <span>Landing Page</span>
+//    <div class="buttons">
+//      <a target="_blank" href="${project.githubLink}">GitHub</a>
+//      <a target="_blank" href="${project.liveDemoLink}">Live Demo</a>
+//    </div>
+//  </div>
+//    `
