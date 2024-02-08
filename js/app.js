48c5eab7680f52489c5bbb82d9faccc5b6e587cb
Suliman Hakimi <122719578+SulimanHakimi@users.noreply.github.com>
2024-02-08 01:32:20 -0800

    add decription of posts

--- js/app.js
+++ js/app.js
@@ -108,7 +108,7 @@ let posts = [
     dateOfPost: "FEB 03,2024",
     imageLink: "./img/sulimanhakimiChess.jpeg",
     description:
-      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+      "I had the opportunity to play in the Kabul Elite Chess Tournament, a fast-paced chess event attended by the best players in Kabul.  It was a challenging and rewarding experience as I faced opponents with different styles and strategies. I did not perform well in these matches, I won 3 games and lost the other 3.  I learned a lot from my mistakes and successes and improved my chess skills.  I also enjoyed the friendly atmosphere and camaraderie between the players. One of the most memorable moments of this tournament was meeting a young boy who was only 8 years old.  He played chess very well and impressed me with his talent and passion.  He was one of the rising stars of the Kabul chess scene and I hope to see him again in future tournaments.",
   },
   {
     id: 2,
@@ -117,7 +117,7 @@ let posts = [
     imageLink:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0xh4e6k0HmfoL5xvlN0jP52fCzvZUNIeJLg&usqp=CAU",
     description:
-      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+      "I am proud to announce that I have reached the 7th place in the Cheragh University Medical terminology exam. It was a national and online exam. The entrance exam was held in four separate rounds, where I was among the top 10 students from the hundreds of participants in different medical fields and institutes. This achievement is the result of my hard work, dedication and passion for learning. I like to thank myself",
   },
   {
     id: 3,
@@ -126,7 +126,7 @@ let posts = [
     imageLink:
       "https://scontent.fkbl6-1.fna.fbcdn.net/v/t39.30808-6/383165889_122137072310007389_7666527922382381436_n.jpg?stp=cp6_dst-jpg&_nc_cat=100&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=voueLqxPecIAX--m2Ua&_nc_ht=scontent.fkbl6-1.fna&oh=00_AfArLyFLHUFnVbeFlmHRbDtDd5ISF76xMgxgh-UjpzErDg&oe=65C91DE1",
     description:
-      "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+      "I participated in the sixth round of the ICPC Asia West Continent Championship, a prestigious programming contest for college students ¹. Out of 50 teams, I ranked 23rd What made this experience even more remarkable was that I was the only person who did not have a computer science background among all the participants. This shows my passion, dedication, and talent for programming and problem-solving. I am proud of my accomplishment and eager to learn more and improve my skills.",
   },
 ];
 for (let i = 0; i < posts.length; i++) {
